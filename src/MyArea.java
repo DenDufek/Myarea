3b9e91319c7fa3fc7e200b9c4e080159276fe7a2
Den <denensogb@gmail.com>
2023-10-30 16:50:05 +0200

    Initial commit

---
+++ src/MyArea.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class MyArea {
+    public static final double PI = 3.14;
+
+    public static double areaOfCircle(double radius) {
+        return PI * radius * radius;
+    }
+}
